2dcfdeaf9201645a384987c0a351eff620afb940
John Marin <john.marin@amount.com>
2021-10-27 21:18:34 +0000

    test upload 2

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,12 +1,12 @@
 
 from setuptools import setup
 
-setup(name='command_line_outlier_detector',
+setup(name='command_line_outlier_detection',
       version='0.1',
       description='random cut forest and isolation forest ensemble',
       # url='http://github.com/storborg/funniest',
       author='John Marin',
       author_email='john.marin@amount.com',
-      packages=['command_line_outlier_detector'],
+      packages=['command_line_outlier_detection'],
       # zip_safe=False
 )
